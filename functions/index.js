0f0a867e862385856416500954729e99cf5f93c3
Caden Weaver <caden.weaver@gmail.com>
2019-11-23 10:38:43 -0800

    lol

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const express = require('express');
+const engines = require('consolidate');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
